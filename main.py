090b87e7b25ddb8203e20e669d07d8871b0fce23
Victor Santos Silva <dadoscomcafe.dev@gmail.com>
2026-06-08 22:30:49 -0300

    (main.py): Exibe o dataframe e seus tipos

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import streamlit as st
 import pandas as pd
 
-situacao = "Aguardando..."
 st.title("Agente Formatador")
 uploaded_file = st.file_uploader("Arquivo a ser formatado!", type=["csv", "xls", "xlsx"])
-st.text(situacao)
+
 
 if uploaded_file is not None:
-    situacao = "Formatando..."#entender como eu apago o texto anterior
-    st.text(uploaded_file.type)
+    situacao = "Formatando..."
+    
     if uploaded_file.type == "text/csv":
         df = pd.read_csv(uploaded_file)
-        print(df.keys())
-        st.table(df)
+    else:
+        df = pd.read_excel(uploaded_file)
+    
+    #st.subheader("Formatação atual:")
+    #st.dataframe(df, width="stretch")
+    
+    st.subheader("Informações das colunas:")
+    info_colunas = []
+    for col in df.columns:
+        info_colunas.append({
+            "Coluna": col,
+            "Formato": str(df[col].dtype)
+        })
+    df_info = pd.DataFrame(info_colunas)
+    st.dataframe(df_info, hide_index=True)
